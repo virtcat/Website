e282f6797af97ee9184437802f6d2761b659129a
VirtCat <i@awsl.link>
2024-07-28 02:40:56 +0800

    Update styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,11 +16,11 @@ defineProps<{
 
     <div style="height: 24px;"></div>
 
-    <h3><a href="https://space.bilibili.com/445016511">哔哩哔哩主页</a></h3>
-    <h3><a href="https://live.bilibili.com/21512984">哔哩哔哩直播间</a></h3>
-    <h3><a style="cursor: not-allowed; filter: grayscale(0.5)">GitHub</a></h3>
-    <h3><a style="cursor: not-allowed; filter: grayscale(0.5)">Pixiv</a></h3>
-    <h3><a href="mailto:i@awsl.link">Email</a></h3>
+    <a class="link" target="_blank" href="https://space.bilibili.com/445016511">哔哩哔哩主页</a>
+    <a class="link" target="_blank" href="https://live.bilibili.com/21512984">哔哩哔哩直播间</a>
+    <a class="link" target="_blank" style="font-style: italic; cursor: not-allowed; filter: grayscale(0.5)">GitHub</a>
+    <a class="link" target="_blank" style="font-style: italic; cursor: not-allowed; filter: grayscale(0.5)">Pixiv</a>
+    <a class="link" target="_blank" href="mailto:i@awsl.link">Email</a>
   </div>
 </template>
 
@@ -41,10 +41,26 @@ h3 {
   text-align: center;
 }
 
+.link {
+  display: block;
+  width: 180px;
+  text-align: center;
+  font-size: 1.2rem;
+  height: 2.4rem;
+  border-radius: 1.2rem;
+  border: 2px solid hsla(300, 80%, 50%, 1);
+  box-sizing: border-box;
+  margin: 8px auto;
+}
+
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
     text-align: left;
+  }
+
+  .link {
+    margin: 8px 0;
   }
 }
 </style>
